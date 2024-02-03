c35e46f30497354b35b04d89790b1584e892c99a
Jarkko Stålnacke <136805206+jlostaln@users.noreply.github.com>
2024-02-03 15:14:35 +0200

    static(dist), request testit Renderille ja UI deploy scriptit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 // esimerkki morganin dokumentaatiosta: morgan.token('type', function (req, res) { return req.headers['content-type'] })
